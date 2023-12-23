4ad44252fdc634354cbbda06694910de372eb199
KAMGA <tetakamgasteve@gmail.com>
2023-12-23 02:20:27 -0500

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from './pages/Profile';
+import About from "./pages/About";
+
+
+
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold ">
-      App
-    </h1>
+    <BrowserRouter>
+      
+      <Routes>
+        <Route path="/" element= {<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
