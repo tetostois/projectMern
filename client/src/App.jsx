06ccceae992910a12bdf8d0fdd45e8ea65e60ac9
KAMGA <tetakamgasteve@gmail.com>
2023-12-23 02:39:26 -0500

    create header components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from './pages/Profile';
 import About from "./pages/About";
-
+import Header from "./components/Header";
 
 
 
@@ -12,7 +12,7 @@ import About from "./pages/About";
 export default function App() {
   return (
     <BrowserRouter>
-      
+      <Header />
       <Routes>
         <Route path="/" element= {<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
